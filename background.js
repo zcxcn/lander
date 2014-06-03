ddf057a6d827c1e30b47cbb24b8e04a9c45744e0
zcxcn <zcxcn@163.com>
2014-06-03 23:10:47 +0800

    content_script

--- background.js
+++ background.js
@@ -1,2 +1,1 @@
 console.log('welcome!!!')
-console.log(chrome.tabs);
--- popup.js
+++ popup.js
@@ -1,1 +1,11 @@
 console.log('i\'m popup.js');
+
+// chrome.browserAction.onClicked.addListener(function(tab) {
+// 	chrome.tabs.executeScript(null, {
+// 		code: "document.body.bgColor='red';window.alert('fuck');"
+// 	});
+// });
+chrome.tabs.executeScript(null, {
+	code: "document.head.appendChild(document.createElement('script'))"
+});
+
